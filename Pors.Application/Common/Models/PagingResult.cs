35c18815d23836a4ca44de85b7638ff32ab531be
Mojtaba Nabavi <dev.nabavi@gmail.com>
2022-03-15 19:54:01 +0330

    Extend paging result model

--- Pors.Application/Common/Models/PagingResult.cs
+++ Pors.Application/Common/Models/PagingResult.cs
@@ -3,15 +3,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
 
 namespace Pors.Application.Common.Models
 {
-    public class PagingResult<T>
+    public class PagingResult
     {
-        public IEnumerable<T> Items { get; set; }
         public int TotalItems { get; set; }
         public int TotalPages { get; set; }
         public int CurrentPage { get; set; }
+        public int ItemsPerPage { get; set; }
+    }
+
+    public class PagingResult<T> : PagingResult
+    {
+        public IEnumerable<T> Items { get; set; }
     }
 }
