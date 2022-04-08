2b4a47e015d53703803ab7961ec0b719ac4c88d9
Mojtaba Nabavi <dev.nabavi@gmail.com>
2022-04-08 21:12:47 +0430

    Adding migration for removing description field from attempt answer entity

---
+++ Pors.Infrastructure/Persistence/Migrations/20220408164023_remove_description_field_from_attempt_answer_table.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pors.Infrastructure.Persistence.Migrations
+{
+    public partial class remove_description_field_from_attempt_answer_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "AttemptAnswers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "AttemptAnswers",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true);
+        }
+    }
+}
